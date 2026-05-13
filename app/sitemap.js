b292de0618c045bfa75e5a5b34949fd5f2b5ca82
luxoasisadvisoryservices-oss <luxoasisadvisoryservices@gmail.com>
2026-05-13 21:04:32 +0400

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,7 @@
+export default function sitemap() {
+  return [
+    { url: 'https://www.oasisrevenuelab.com', lastModified: new Date() },
+    { url: 'https://www.oasisrevenuelab.com/privacy', lastModified: new Date() },
+    { url: 'https://www.oasisrevenuelab.com/terms', lastModified: new Date() },
+  ]
+}
